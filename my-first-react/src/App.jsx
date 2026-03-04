d11b801d20b7bbeaec06fe2a520a2c2dc91c0b9a
lydieuwayezu <uwayezulydie1w@gmail.com>
2026-03-04 16:06:03 +0200

    adding the photoes

---
+++ my-first-react/src/App.jsx
@@ -0,0 +1,25 @@
+import IdCard from './components/IdCard';
+
+function App() {
+  const employees = [
+    {
+      id: 1,
+      name: "Liliane nyakayiru",
+      role: "Software Engineer",
+      department: "web designer",
+      avatar: "/images/person1.PNG"
+    },
+    {
+      id: 2,
+      name: "lydie uwayezu",
+      role: "Product Manager",
+      department: "Product",
+      avatar: "/images/person2.jpg"  // Put your image in public/images/
+    },
+    {
+      id: 3,
+      name: "Lydie uwaayezu",
+      role: "front-end developer",
+      department: "Design",
+      avatar: "/images/person3.jpg"  // Put your image in public/images/
+    }
